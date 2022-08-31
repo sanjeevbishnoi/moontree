bb1a776ac6c682edaa6b45e006e3b7132443217e
Jordan Miller <Jordan.kay@gmail.com>
2022-08-30 21:49:28 -0600

    #43 refactor tests for readability

--- test/unit/data_model/wallet.dart
+++ test/unit/data_model/wallet.dart
@@ -1,27 +1,17 @@
 // ignore_for_file: avoid_print
 import 'package:flutter_test/flutter_test.dart';
-import 'package:moontree/foundation/data_model/records/records.dart';
 import 'package:moontree/foundation/data_model/proclaim/proclaim.dart' as data;
 import 'package:moontree/foundation/utils/derivation.dart';
 import 'package:moontree/foundation/services/derivation.dart'
     show DerivationProcessor;
 
 import '../../fixtures/data_model/fixtures.dart';
+import '../../fixtures/data_model/sets/wallet_address.dart';
 
 void main() {
   group('wallet', () {
-    final mnemonic =
-        'strike faint hockey bean average crazy carry spatial hammer glow sorry fiscal';
-    final wallet = WalletDeviceRecord(
-      pubkey:
-          '03995ebd7d84f6c435391035f45a44cf6bd1d8c0ac627233706d0b0ad0f49a2c6d',
-      derivation: "m/44'/175'/0'/0",
-      name: 'name',
-      mnemonic: mnemonic,
-      privkey:
-          '4a72457e41c65f084a798649c3918dd76744baff6023bd3b0f5878fc01c45a7c',
-    );
-
+    final wallet = WalletWithAddress.wallet;
+    final mnemonic = WalletWithAddress.mnemonic;
     setUp(() {});
 
     test('create data_model wallet', () async {
--- test/unit/domain_model/wallet.dart
+++ test/unit/domain_model/wallet.dart
@@ -7,24 +7,15 @@ import 'package:moontree/foundation/data_model/records/records.dart';
 import 'package:moontree/foundation/domain_model/proclaim/proclaim.dart'
     as domain;
 
+import '../../fixtures/data_model/sets/wallet_address.dart';
 import '../../fixtures/domain_model/fixtures.dart';
 
 void main() {
   group('wallet', () {
-    final mnemonic =
-        'strike faint hockey bean average crazy carry spatial hammer glow sorry fiscal';
+    final wallet = WalletWithAddress.wallet;
     setUp(() {});
     test('data_model wallet to domain_wallet with given derivation', () async {
       useFixture();
-      final wallet = WalletDeviceRecord(
-        pubkey:
-            '03995ebd7d84f6c435391035f45a44cf6bd1d8c0ac627233706d0b0ad0f49a2c6d',
-        derivation: "m/44'/175'/0'/0",
-        name: 'name',
-        mnemonic: mnemonic,
-        privkey:
-            '4a72457e41c65f084a798649c3918dd76744baff6023bd3b0f5878fc01c45a7c',
-      );
       final call = () => domain.wallets.byWallet.getOne(wallet.pubkey);
       expect(call(), isNull);
       await ToWalletDomain.load(wallet);
@@ -35,13 +26,11 @@ void main() {
         () async {
       useFixture();
       final walletCustom = WalletDeviceRecord(
-        pubkey:
-            '03995ebd7d84f6c435391035f45a44cf6bd1d8c0ac627233706d0b0ad0f49a2c6d',
+        pubkey: wallet.pubkey,
         derivation: "m/44'/0",
-        name: 'name',
-        mnemonic: mnemonic,
-        privkey:
-            '4a72457e41c65f084a798649c3918dd76744baff6023bd3b0f5878fc01c45a7c',
+        name: wallet.name,
+        mnemonic: wallet.mnemonic,
+        privkey: wallet.privkey,
       );
       final call = () => domain.wallets.byWallet.getOne(walletCustom.pubkey);
       expect(call(), isNull);
